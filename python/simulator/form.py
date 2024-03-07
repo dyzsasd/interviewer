f063521aa0599dfc42c114b4b42c245071bc56b8
Shuai ZHANG <dyzsasd@gmail.com>
2024-03-07 14:58:53 +0100

    check uploaded files

--- python/simulator/form.py
+++ python/simulator/form.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import InterviewDocument
 
 
@@ -6,3 +7,19 @@ class InterviewDocumentForm(forms.ModelForm):
     class Meta:
         model = InterviewDocument
         fields = ['language', 'cv', 'job_description']
+    
+    def clean_cv(self):
+        cv = self.cleaned_data.get('cv')
+        if not cv.name.endswith('.pdf'):
+            raise ValidationError("Invalid file type for CV. PDF required.")
+        if cv.size > 1024*1024*5: # 5MB limit
+            raise ValidationError("CV file too large. Size should not exceed 5MB.")
+        return cv
+
+    def clean_job_description(self):
+        job_description = self.cleaned_data.get('job_description')
+        if not job_description.name.endswith('.pdf'):
+            raise ValidationError("Invalid file type for job description. PDF required.")
+        if job_description.size > 1024*1024*5: # 5MB limit
+            raise ValidationError("Job description file too large. Size should not exceed 5MB.")
+        return job_description
